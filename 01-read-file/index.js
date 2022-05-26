b9ddfb157106175a691fe7daa81492570bba2e01
0iskak <isnu260102@gmail.com>
2022-05-26 18:50:23 +0600

    fix: change ReadFile to ReadStream

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,6 +2,4 @@ const fs = require('node:fs');
 const path = require('node:path');
 const process = require('node:process');
 
-fs.readFile(path.join(__dirname, 'text.txt'), (err, data) => {
-  process.stdout.write(data);
-});
+fs.createReadStream(path.join(__dirname, 'text.txt')).pipe(process.stdout);
